0dec7c6be2dea5660c31b70805e46a5f0ecf317c
denbit <den_b_it@i.ua>
2018-11-24 12:25:22 +0200

    dll exp

---
+++ ParP/Header.h
@@ -0,0 +1,3 @@
+#pragma once
+#pragma once
+__declspec(dllimport) void DisplayHelloFromMyDLL();
--- ParP/main.c
+++ ParP/main.c
@@ -2,14 +2,17 @@
 
 #include<stdio.h>
 #include <windows.h>
+#include "Header.h"
 
 int main(int argc, char* argv[]) {
-	printf(" count is %d \n", argc);
+	/*printf(" count is %d \n", argc);
 	for (int i = 0; i < argc; i++) {
 		puts(argv + i);
 	}
-	gets();
+	gets();*/
 	
+	DisplayHelloFromMyDLL();
+	gets();
 
 	return 0;
 }
